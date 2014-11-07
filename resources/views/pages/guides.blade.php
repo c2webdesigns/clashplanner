0254dcb7de3fd5a55194e5367874c26611c863b5
Corey Coburn <corey@c2webdesigns.com>
2014-11-07 14:45:40 -0800

    Installed Clash web font

--- resources/views/pages/guides.blade.php
+++ resources/views/pages/guides.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.master')
 
 @section('content')
-
-<div class="container container-first">
-    <p>This is where the guides will go!</p>
-</div>
+    <div class="container container-first">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Guides</h1>
+                <p>Guides will go here</p>
+            </div>
+        </div>
+    </div>
 @stop
--- resources/views/wars/index.blade.php
+++ resources/views/wars/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <div class="container container-first">
-    asdf
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Wars</h1>
+                <p>War list will go here</p>
+            </div>
+        </div>
     </div>
 @stop
