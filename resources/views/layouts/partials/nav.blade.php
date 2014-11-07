f953a3cf67f989b67d138c8e09d90f418689f8c4
Corey Coburn <corey@c2webdesigns.com>
2014-11-07 14:03:03 -0800

    Fixed responsive navigation

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -2,47 +2,43 @@
 
   <div class="container container-nav">
     <div class="row">
-      <div class="col-sm-6 navbar-header">
-        <a class="navbar-brand" href="/"><img src="/images/logo-small.png" class="logo-nav" alt="logo brand"/></a>
-      </div>
+      <div class="col-md-12 navbar-header">
+        <a class="navbar-brand" href="/"><img src="/images/logo.png" class="logo-nav" alt="logo brand"/></a>
 
-      <div class="col-sm-6">
+        <div class="signin-toggle pull-right">
 
-        <div class="signin-toggle pull-right" style="bottom: green;">
+          @if (Auth::check())
 
-        @if (Auth::check())
-        
-          <img src="{{ Auth::user()->avatar }}" class="user-avatar img-circle hidden-xs" alt="user avatar"/>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown settings-nav">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ \App\Helpers::displayName() }} <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Stats</a></li>
-                <li><a href="#">Contact Leaders</a></li>
-                <li class="divider"></li>
-                <li><a href="{{ route('logout_path') }}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-              </ul>
-            </li>
-          </ul>
+            <img src="{{ Auth::user()->avatar }}" class="user-avatar img-circle hidden-xs" alt="user avatar"/>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown settings-nav">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ \App\Helpers::displayName() }} <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Profile</a></li>
+                  <li><a href="#">Stats</a></li>
+                  <li><a href="#">Contact Leaders</a></li>
+                  <li class="divider"></li>
+                  <li><a href="{{ route('logout_path') }}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                </ul>
+              </li>
+            </ul>
 
-        @else
+          @else
 
-          <a class="btn btn-success" id="btn-signin" href="/auth/login">Sign in</a>
+            <a class="btn btn-success" id="btn-signin" href="/auth/login">Sign in</a>
 
-        @endif
+          @endif
 
         </div>
 
       </div>
-
     </div>
   </div>
 
   @if(Auth::check())
   <nav class="main-navigation">
     <div class="container">
-        <button type="button" class="navbar-toggle collapsed navbar-inverse" data-toggle="collapse" data-target="#main-nav-links">
+        <button type="button" class="navbar-toggle collapsed navbar" data-toggle="collapse" data-target="#main-nav-links">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
